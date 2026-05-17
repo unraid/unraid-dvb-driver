258ff07db75b3e51f54f7af21ae3fc6988a53ab8
Christoph <christophhummer@gmail.com>
2026-05-17 12:51:26 +0200

    Update compile-tbsos.sh
    - fix variable issues

--- source/compile-tbsos.sh
+++ source/compile-tbsos.sh
@@ -9,7 +9,7 @@ cd ${DATA_DIR}/TBS
 BASE_URL="https://www.tbsiptv.com/download/common/"
 LATEST_FILE=$(curl -s 'https://www.tbsiptv.com/index.php?route=product/download/search&dkeyword=Linux+Driver+Beta' | grep -oE 'tbsdvb_v[0-9]+\.tar\.bz2')
 FULL_URL="${BASE_URL}${LATEST_FILE}"
-PLG_VERSION="$(echo "$LATEST_FILE" | cut -d'_' -f2 | cut -d'.' -f1 | tr -d 'v')"
+PLUGIN_VERSION="$(echo "$LATEST_FILE" | cut -d'_' -f2 | cut -d'.' -f1 | tr -d 'v')"
 
 # Download package
 wget -O ${DATA_DIR}/TBS/tbs.tar.bz2 "$FULL_URL"
@@ -21,7 +21,7 @@ make -j${CPU_COUNT} CONFIG_DVB_STB6100=m KCFLAGS="-DCONFIG_MEDIA_TUNER_TDA18271_
 make install -j${CPU_COUNT} MDIR=/tbs CONFIG_DVB_STB6100=m KCFLAGS="-DCONFIG_MEDIA_TUNER_TDA18271_MODULE=1 -DCONFIG_MEDIA_TUNER_TDA8290_MODULE=1 -DCONFIG_DVB_STV6110x_MODULE=1 -DCONFIG_DVB_STV6111_MODULE=1"
 
 # Extract firmware files
-tar -xf tbs-tuner-firmwares_v1.0.tar.bz2 -C /tbs/lib/firmware/
+tar -xf tbs-tuner-firmwares_*.tar.bz2 -C /tbs/lib/firmware/
 
 # Remove unnecessary files from moudles directory
 cd /tbs/lib/modules/${UNAME}/
@@ -32,7 +32,7 @@ find . -depth -exec rmdir {} \;  2>/dev/null
 PLUGIN_NAME="tbsos"
 BASE_DIR="/tbs"
 TMP_DIR="/tmp/${PLUGIN_NAME}_"$(echo $RANDOM)""
-VERSION="$PLG_VERSION"
+VERSION="$PLUGIN_VERSION"
 
 mkdir -p $TMP_DIR/$VERSION
 cd $TMP_DIR/$VERSION
@@ -46,5 +46,5 @@ $PLUGIN_NAME:
 $PLUGIN_NAME: Custom $PLUGIN_NAME DVB driver package for Unraid Kernel v${UNAME%%-*} by ich777
 $PLUGIN_NAME:
 EOF
-${DATA_DIR}/bzroot-extracted-$UNAME/sbin/makepkg -l n -c n $TMP_DIR/$PLUGIN_NAME-plugin-$UNAME-1.txz
-md5sum $TMP_DIR/$PLUGIN_NAME-plugin-$UNAME-1.txz | awk '{print $1}' > $TMP_DIR/$PLUGIN_NAME-plugin-$UNAME-1.txz.md5
+${DATA_DIR}/bzroot-extracted-$UNAME/sbin/makepkg -l n -c n $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz
+md5sum $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz | awk '{print $1}' > $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz.md5
